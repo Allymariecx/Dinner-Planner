4c56f51489c564f7f3a0b41847887d93d850fbc9
Allymariecx <whvgm77pcc@privaterelay.appleid.com>
2026-05-09 20:21:02 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // fooendar service worker
 // Bump CACHE_VERSION when you deploy a new version of the HTML
-const CACHE_VERSION = 'fooendar-v2';
+const CACHE_VERSION = 'fooendar-v3';
 const ASSETS = [
   '/Dinner-Planner/index.html',
   '/Dinner-Planner/manifest.json',
